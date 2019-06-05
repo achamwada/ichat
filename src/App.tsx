4f7b6d1570da79473006afb4a5ff3f8be2ab2109
Alexander Chamwada <alexanderchamwada@gmail.com>
2019-06-05 10:14:36 +0100

    cleaning

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,8 @@
 import React from "react";
-import { BrowserRouter as Router, Route, Link } from "react-router-dom";
+import { BrowserRouter as Router, Route } from "react-router-dom";
 import "./App.css";
 import Dashboard from "./components/dashboard/Dashboard";
-import Store, { ContextStore } from "./store/store";
+import Store from "./store/store";
 import Login from "./components/authentication/Login";
 
 const App: React.FC = () => {
